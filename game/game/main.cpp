3507ba9194226ec74b85a41e289490ba45726d4f
Bawar Jalal <bawar96@hotmail.com>
2016-07-05 18:04:45 +0100

    new example

--- game/game/main.cpp
+++ game/game/main.cpp
@@ -2,23 +2,30 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	// create the window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	// run the program as long as the window is open
+	while (window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+		// clear the window with black color
+		window.clear(sf::Color::Black);
 
-    return 0;
+		// draw everything here...
+		// window.draw(...);
+
+		// end the current frame
+		window.display();
+	}
+
+	return 0;
 }
